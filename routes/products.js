35dbff22487fecf241c93b135464e8aae0094818
Shareef Dweikat <sdweikat@asaltech.com>
2023-04-30 09:53:15 +0200

    Add product router

---
+++ routes/products.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const {
+    getProducts,
+    createProduct,
+} = require('../controllers/products')
+
+const router = express.Router()
+
+router.route('/')
+    .get(getProducts)
+    .post(createProduct)
+
+module.exports = router
