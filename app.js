39c148e7804e68c179c90eb7c307d1c1d172556f
Shareef Dweikat <sdweikat@asaltech.com>
2023-04-30 13:12:01 +0300

    Add static files support

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 const dotenv = require('dotenv');
 dotenv.config({path: './config/config.env'});
 
@@ -7,12 +9,13 @@ const connectDB = require('./config/db')
 const colors = require('colors');
 // const errorHandler = require('./middlewares/error');
 
-
 connectDB();
 
 const app = express()
 
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json())
+
 app.use('/api/v1/products/', products)
 
 // app.use(errorHandler)
