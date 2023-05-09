2fdca666c1b1e118d1689aea4727c1c2b30cd53c
Shareef Dweikat <sdweikat@asaltech.com>
2023-05-09 16:11:55 +0300

    Add user APIs

---
+++ routes/users.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const {
+  getUser,
+  createUser,
+  updateUser,
+  deleteUser
+} = require('../controllers/users');
+
+const User = require('../models/User');
+
+const router = express.Router({ mergeParams: true });
+
+router
+  .route('/')
+  .post(createUser);
+
+router
+  .route('/:id')
+  .get(getUser)
+  .put(updateUser)
+  .delete(deleteUser);
+
+module.exports = router;
