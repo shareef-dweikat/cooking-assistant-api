b2a230631a9d999de95c80c5bf7a09ea8b3a6398
Shareef Dweikat <sdweikat@asaltech.com>
2023-05-14 10:11:53 +0300

    Add auth for assistant  endpoint

--- routes/assistant.js
+++ routes/assistant.js
@@ -2,8 +2,11 @@ const express = require('express');
 const {
     query
 } = require('../controllers/assistant')
+const { protect } = require('../middlewares/auth');
 
 const router = express.Router()
+
+router.use(protect);
 
 router.route('/')
     .post(query)
