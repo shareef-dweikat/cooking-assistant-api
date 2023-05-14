6d74eaa2b5cce71bca9f9d6eb580f2f9fa35ce2d
Shareef Dweikat <sdweikat@asaltech.com>
2023-05-14 10:11:37 +0300

    Add auth for application endpoint

--- routes/applications.js
+++ routes/applications.js
@@ -2,20 +2,24 @@ const express = require('express');
 const {
     getApplications,
     createApplication,
-    getApplication,
-    updateApplication,
-    deleteApplication
+    // getApplication,
+    // updateApplication,
+    // deleteApplication
 } = require('../controllers/applications')
 
+const { protect, authorize } = require('../middlewares/auth');
+
 const router = express.Router()
+
+router.use(protect);
 
 router.route('/')
     .get(getApplications)
     .post(createApplication)
 
-router.route('/:id')
-    .get(getApplication)
-    .put(updateApplication)
-    .delete(deleteApplication)
+// router.route('/:id')
+//     .get(getApplication)
+//     .put(updateApplication)
+//     .delete(deleteApplication)
 
 module.exports = router
